254816848ae7ff6adec0b7fb6bf0b3a606b1b3f3
nyon3 <tomoya@crosstalk.me>
2023-08-29 18:56:17 +0900

    Added conversation history

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# line-chatbot-crosstalk
-a test bot
+# ChatGPT Chatbot for LINE on Firebase
+
+## Overview
+
+This project integrates ChatGPT (using OpenAI's API) into a LINE chatbot deployed on Firebase. This chatbot can respond to text messages from LINE users and manage conversation history.
+
+## Prerequisites
+
+- Node.js
+- npm
+- Firebase CLI
+- ngrok
+- OpenAI API Key
+- LINE Developer Account
+
+## Setup
+
+1. **Clone the Repository**
+
+   ```bash
+   git clone <repository_url>
+   cd <repository_folder>
+   ```
+
+2. **Install Dependencies**
+
+   ```bash
+   npm install
+   ```
+
+3. **Set Environment Variables**
+
+    Create a `.env` file and set the following variables:
+    - `OPENAI_SECRET_KEY`
+    - `CHANNEL_ACCESS_TOKEN`
+    - `CHANNEL_SECRET`
+
+4. **Initialize Firebase**
+
+   ```bash
+   firebase init
+   ```
+
+   Follow the prompts to configure your Firebase project.
+
+## Local Development
+
+1. **Start the Development Server**
+
+    ```bash
+    firebase serve --only functions
+    ```
+
+2. **Expose Local Server using Ngrok**
+
+    Open a new terminal and run:
+
+    ```bash
+    ngrok http 5001
+    ```
+
+    Note: Replace `5001` with the port number your Firebase functions are running on.
+
+3. **Configure Webhook URL on LINE**
+
+    - Go to your LINE Developer console.
+    - Update the webhook URL to the forwarding URL provided by ngrok (e.g., `https://<your_ngrok_id>.ngrok.io/<project_path>/webhook`).
+
+## Usage
+
+Send text messages to your LINE bot. It will generate responses using the ChatGPT model and reply.
+
+## Debugging
+
+To check the chat history or any variables:
+
+- Use `console.log()` in your functions to log values.
+- Use the Firebase Functions logs to view real-time logs.
+
+## License
+
+TBD
+
+---
+
+Feel free to add more sections or details as you find necessary.
--- functions/index.js
+++ functions/index.js
@@ -4,31 +4,43 @@ const express = require("express");
 const line = require("@line/bot-sdk");
 const {Configuration, OpenAIApi} = require("openai");
 
+// Define runtime options for Firebase Functions
 const runtimeOpts = {
   secrets: ["OPENAI_SECRET_KEY", "CHANNEL_ACCESS_TOKEN", "CHANNEL_SECRET"],
 };
 
+// Create a new OpenAI configuration object
 const configuration = new Configuration({
   apiKey: `${process.env.OPENAI_SECRET_KEY}`,
 });
+
+// Create a new OpenAI API object using the configuration
 const openai = new OpenAIApi(configuration);
+
+// Initialize the Firebase Admin SDK
 admin.initializeApp();
 
+// Create a new Express app
 const app = express();
+
+// Define configuration options for the LINE SDK
 const config = {
   channelAccessToken: `${process.env.CHANNEL_ACCESS_TOKEN}`,
   channelSecret: `${process.env.CHANNEL_SECRET}`,
 };
+
+// Create a new LINE client using the configuration
 const client = new line.Client(config);
 
-// eslint-disable-next-line no-undef
+// Define a route for the root URL
 app.get("/", (req, res) => {
-  // eslint-disable-next-line max-len
   res.send(`Hello from Firebase!!!`);
 });
 
+// Define a route for the LINE webhook
 app.post("/webhook", async (req, res) => {
   try {
+    // Handle each event in parallel
     await Promise.all(req.body.events.map(handleEvent));
     res.json({message: "success"});
   } catch (error) {
@@ -37,24 +49,114 @@ app.post("/webhook", async (req, res) => {
   }
 });
 
-// eslint-disable-next-line require-jsdoc
-async function getAIResponse(prompt) {
-  // eslint-disable-next-line no-async-promise-executor
+// Define the maximum number of tokens allowed in a message
+const maxTokens = 4096;
+
+// Define an array to store the conversation history
+const conversationHistory = [];
+
+// Define a system prompt to use in the conversation
+const systemPrompt = "あなたは役に立つAIロボ";
+
+/**
+ * Counts the number of tokens in a given message.
+ * Tokens are considered to be separated by spaces.
+ *
+ * @param {string} message - The message to tokenize.
+ * @return {number} - The number of tokens in the message.
+ */
+function countTokens(message) {
+  return message.split("").length;
+}
+
+/**
+ * Summarizes the conversation history using ChatGPT.
+ *
+ * @async
+ * @param {Array} history - The conversation history to summarize.
+ * @return {Promise<Array>} - An array containing a summarized system message.
+ */
+async function summarizeConversation(history) {
   try {
+    const messages = [...history];
+    messages.unshift(
+        {role: "system", content: systemPrompt},
+        {
+          role: "assistant",
+          content:
+          "これまでの会話をアタナだけがわかるように、できるだけ手短に要約してください",
+        },
+    );
+
+    const summary = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages,
+    });
+
+    return [
+      {role: "system", content: summary.data.choices[0].message.content},
+    ];
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+/**
+ * Generates an AI response to a given prompt using ChatGPT.
+ *
+ * @async
+ * @param {string} prompt - The user's prompt.
+ * @return {Promise<string|null>} - A promise that resolves to the AI response.
+ */
+async function getAIResponse(prompt) {
+  try {
+    // Add the new user message to the conversation history
+    conversationHistory.push({role: "user", content: prompt});
+
+    // Check if the conversation history exceeds the token limit
+    const tokenCount = conversationHistory.reduce(
+        (acc, msg) => acc + countTokens(msg.content),
+        0,
+    );
+
+    let messages = [];
+    if (tokenCount > maxTokens) {
+      // If so, summarize the conversation, and begin a new conversation
+      messages = await summarizeConversation(conversationHistory);
+      conversationHistory.length = 0; // Clear the existing history
+      conversationHistory.push(...messages); // Add the summary to the history
+    } else {
+      messages = [...conversationHistory];
+    }
+    messages.unshift({role: "system", content: systemPrompt});
+
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: [
-        {role: "system", content: "あなたは英会話の先生です、英会話を学び始めた初心者向けに簡単に英吾を教えてください。"},
-        {role: "user", content: prompt},
-      ],
+      messages,
     });
+
+    // Add the new assistant message to the conversation history
+    conversationHistory.push({
+      role: "assistant",
+      content: completion.data.choices[0].message.content,
+    });
+    console.log("Updated chat history: " + JSON.stringify(conversationHistory, null, 2));
+
     return completion.data.choices[0].message.content;
   } catch (error) {
     console.error(error);
     return null;
   }
 }
-// eslint-disable-next-line require-jsdoc
+
+/**
+ * Handles an event from the LINE webhook.
+ *
+ * @async
+ * @function handleEvent
+ * @param {Object} event - The event object.
+ * @return {Promise<Object|null>} - A promise that resolves to the response
+ */
 async function handleEvent(event) {
   if (event.type !== "message" || event.message.type !== "text") {
     return Promise.resolve(null);
@@ -78,6 +180,5 @@ async function handleEvent(event) {
   }
 }
 
-
-// eslint-disable-next-line no-undef
+// Export the Express app as a Firebase Function
 exports.api = functions.runWith(runtimeOpts).https.onRequest(app);
